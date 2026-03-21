056cd3f710d9d45b228c77cde504778e781b914b
polo-omez <pablogomez0904@gmail.com>
2026-03-22 00:00:28 +0100

    Backup automático: 2026-03-22 00:00:28

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -4,14 +4,14 @@ return {
 	event = "VeryLazy",
 	keys = {
 		{
-			"<leader>se",
+			"<leader>e",
 			mode = { "n", "v" },
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
 		},
 		{
 			-- Open in the current working directory
-			"<leader>e",
+			"<leader>se",
 			"<cmd>Yazi cwd<cr>",
 			desc = "Open the file manager in nvim's working directory",
 		},
