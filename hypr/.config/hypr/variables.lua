03508340251ef569425935879aa973afa5d9a90e
polo-omez <pablogomez0904@gmail.com>
2026-06-12 00:00:21 +0200

    Backup automático: 2026-06-12 00:00:21

--- hypr/.config/hypr/variables.lua
+++ hypr/.config/hypr/variables.lua
@@ -26,10 +26,10 @@ animSpeedFast = 0.5
 noAnim = 0
 animSpeedSlow = 3
 
-opacityActive = 1
-opacityInactive = 1
---opacityActive = 0.975
---opacityInactive = 0.965
+--opacityActive = 1
+--opacityInactive = 1
+opacityActive = 0.985
+opacityInactive = 0.975
 opacityFloat = 0.93
 opacitySpecial = 0.6
 
